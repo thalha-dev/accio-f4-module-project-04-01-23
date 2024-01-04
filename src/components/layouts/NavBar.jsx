b474aea7e5c65d104794faf99ff51480838b841d
thalha-dev <dev.thalha@gmail.com>
2024-01-04 10:43:02 +0530

    implement navbar and give styles

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
-      <h1>Nav</h1>
+      <header className="bg-black text-white">
+        <nav className="text-center p-[1em] sm:flex sm:items-center sm:justify-between">
+          <h2>Shopping Cart</h2>
+          <ol className="flex gap-[1em] pt-[1em] justify-center sm:pt-0 ">
+            <NavLink to="/">Home Page</NavLink>
+            <NavLink to="/cart">Cart Page</NavLink>
+          </ol>
+        </nav>
+      </header>
       <Outlet />
     </div>
   );
